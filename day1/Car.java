9a3d6f82874a0b261dc43bc7adc7f6e3d67cc2d4
kumaroeb <110729939+kumaroeb@users.noreply.github.com>
2022-08-06 19:38:51 +0530

    Add files via upload
    day1

---
+++ day1/Car.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Car 
+{
+String applyBreak;
+String soundHorn;
+
+public void applyBreak()
+{
+	System.out.println("Applied Break");
+}
+public void soundHorn()
+{
+	System.out.println("Sound horn");
+}
+}
+	
+
+	
+	
+
+
---
+++ day1/Student.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Student {
+	public static void main(String[] args) {
+		String studentName="PraveenKumar";
+	    long rollNo=141051101115l;
+	    String collegeName="DR MGR UNIVERSITY";
+	    short markScored=69;
+	    float ccpa=7.2f;
+	    
+	    System.out.println(studentName);
+	    System.out.println(rollNo);
+	    System.out.println(collegeName);
+	    System.out.println(markScored);
+	    System.out.println(ccpa);
+		
+	}
+
+}
